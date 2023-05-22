34cdb90dd6cf3e1e9fb34ca41e2fd62699f2bbf9
dianprnms <purnamasaridian010@gmail.com>
2023-05-22 20:47:24 +0700

    Pra

---
+++ app/src/main/java/com/kelompok6/view/DetailNews.kt
@@ -0,0 +1,23 @@
+package com.kelompok6.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kelompok6.R
+import com.kelompok6.databinding.FragmentCartBinding
+import com.kelompok6.databinding.FragmentDetailBinding
+
+class DetailNews : Fragment() {
+    lateinit var binding: FragmentDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}

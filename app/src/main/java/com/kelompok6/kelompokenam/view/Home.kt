80b547a27f610580c144ed90e9baef4b58251c33
dianprnms <purnamasaridian010@gmail.com>
2023-05-23 15:20:02 +0700

    Pra

--- app/src/main/java/com/kelompok6/kelompokenam/view/Home.kt
+++ app/src/main/java/com/kelompok6/kelompokenam/view/Home.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kelompok6.R
 import com.kelompok6.databinding.FragmentHomeBinding
 
 
@@ -18,6 +20,14 @@ class Home : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toProfil.setOnClickListener {
+            findNavController().navigate(R.id.action_home2_to_profile)
+
+        }
     }
 
 }
